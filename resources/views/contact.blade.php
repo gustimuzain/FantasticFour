d53ed2c28f7bb9771baef85be531e0871b70032f
gustimuzain <aprilandrigusti@gmail.com>
2019-12-09 22:50:48 +0700

    tambah view contact

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="inter/css/main.css">
+        <link rel="stylesheet" href="inter/css/responsive.css">
+    </head>
+
+    <body>
+        <header>
+            <nav class="navbar bg-white navbar-expand-md shadow p-3 mb-5 fixed-top">
+                <a class="navbar-brand" href="index.html">
+                    <img src="image/logoF4.png" alt="logo" class="logo">
+                </a>
+                <ul class="navbar-nav navbar-center">
+                        <li class="nav-item">
+                            <a class="nav-link" href="">LAYANAN</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">SYARAT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">KONTAK</a>
+                        </li>
+                </ul>
+                <div class="navbar-nav ml-auto">
+                    <a href="daftar.html" class="nav-item nav-link">Login</a>
+                    <a href="masuk.html" class="nav-item nav-link">Daftar</a>
+                </div>
+            </nav>
+        </header>
+        
+        <div class="container">
+            <div class="row">    		
+                <div class="col-sm-12">    			   			
+                    <h2 class="title text-center">Hubungi Kami</h2>    			    				    				
+                    <div class="contact-map">
+                        <div style="width: 100%">
+                            <iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=110.40847328782317, -7.76047945&amp;q=Jl.%20Ring%20Road+(Fantastic%20Four%20Rental%20Car)&amp;ie=UTF8&amp;t=&amp;z=13&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">find my coordinates</a></iframe>
+                        </div><br/>
+                    </div>
+                </div>			 		
+            </div>    	
+             <div class="">  	<!--row -->
+                <div class="col-sm-4">
+                    <h2 class="title">Info</h2>
+                    <address>
+                        <p>Fantastic Four Rental Car</p>
+                        <p>Jl. Ring Road</p>
+                        <p>Yogyakarta, Indonesia</p>
+                    </address>
+                </div>    			
+            </div>	
+        </div>
+        
+        <div class="footer">
+            <div class="contain">
+                <div class="col">
+                    <h1>Tentang</h1>
+                    <h1>Pusat Bantuan</h1>
+                    <h1>Kebijakan</h1>
+                </div>
+                <div class="col social ml-auto">
+                    <h1>Sosial</h1>
+                    <ul>
+                        <li>WhatsApp<img src="" width="32" style="width: 32px;">08977876777</li>
+                        <li>Email<img src="" width="32" style="width: 32px;">fantastic4rent@gmail.com</li>
+                        <li>Instagram<img src="" width="32" style="width: 32px;">fantastic4rent</li>
+                    </ul>
+                </div>
+            <div class="clearfix"></div>
+            </div>
+        </div>
+    </body>
+</html>
