1b1440e42c2b364901cf74c5f179d27550bfb48d
gustimuzain <aprilandrigusti@gmail.com>
2019-12-09 22:52:04 +0700

    tambah view item_detail

---
+++ resources/views/item_detail.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        <link rel="stylesheet" href="inter/css/main.css">
+        <link rel="stylesheet" href="inter/css/responsive.css">
+    </head>
+
+    <body>
+        <header>
+            <nav class="navbar bg-white navbar-expand-md shadow p-3 mb-5 fixed-top">
+                <a class="navbar-brand" href="index.html">
+                    <img src="image/logoF4.png" alt="logo" class="logo">
+                </a>
+                <ul class="navbar-nav navbar-center">
+                        <li class="nav-item">
+                            <a class="nav-link" href="">LAYANAN</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">SYARAT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">KONTAK</a>
+                        </li>
+                </ul>
+                <div class="navbar-nav ml-auto">
+                    <a href="daftar.html" class="nav-item nav-link">Login</a>
+                    <a href="masuk.html" class="nav-item nav-link">Daftar</a>
+                </div>
+            </nav>
+        </header>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card rounded cardshad cardrespo">
+                        <div>
+                            <div class="productdetail">
+                                <img src="inter//Mobil/brio.jpg" alt="">
+                            </div>
+                            <div class="d-flex flex-column align-items-center">
+                                <div class="">
+                                   <h5 class="">Honda Brio</h5>
+                                   <div class="d-flex justify-content-center">
+                                        <span class="badge badge-pill badge-secondary align-self-center">Manual</span>
+                                        <div class="ml-2">
+                                            <span><i class="fas fa-user"></i>4</span>
+                                        </div>
+                                   </div>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="harga">Rp250.000/hari</h5>
+                                </div>                                
+                            </div>
+                        </div>                            
+                    </div>
+                </div>
+            
+                <div class="col-sm-8">
+                    <div class="card cardshad">
+                        <div>
+                            <div class="d-flex align-items-center">
+                                <div class="mt-4 ml-4">
+                                    <h5 class="harga">Rp250.000 / 1 hari</h5>
+                                </div>
+                                <div class="mr-4 mt-3 ml-auto">
+                                    <button type="button" class="btn btn-primary">Pilih</button>
+                                </div>
+                            </div> 
+                            <hr>
+                            <div class="ml-4">
+                                <div>
+                                    <p>Info Mobil</p>
+                                    <ul>
+                                        <li>Tahun 2018</li>
+                                        <li>hhj</li>
+                                        <li>jgfg</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <p>Info Rental</p>
+                                    <ul>
+                                        <li>Tahun 2018</li>
+                                        <li>hhj</li>
+                                        <li>jgfg</li>
+                                    </ul>
+                                </div>
+                                <div>
+                                    <p>Syarat Peminjaman</p>
+                                    <ul>
+                                        <li>Tahun 2018</li>
+                                        <li>hhj</li>
+                                        <li>jgfg</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
